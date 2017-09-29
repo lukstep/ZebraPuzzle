383ce90fa8949acec2104feeec9a5c8f6b5d20cd
Lukasz <lukaszstepien02@gmail.com>
2017-09-29 21:51:06 +0200

    Add function to generate random solution

--- zebraPuzzle.py
+++ zebraPuzzle.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import random
 
 nation = [ "Norwegian", "Ukrainian", "Englishman", "Spaniard", "Japanese" ]
 color  = [ "Red", "Blue", "Yellow", "Ivory", "Green" ]
@@ -7,3 +8,18 @@ smoke  = [ "Lucky Strike", "Old Gold", "Kools", "Chesterfield" ]
 pet    = [ "Zebra", "Fox", "Horse", "Snails", "Dog" ]
 
 
+def generateRandomRecord():
+	record = {"nation" : random.choice(nation), 
+		      "color" : random.choice(color), 
+		  	  "drink" : random.choice(drink), 
+		      "smoke" : random.choice(smoke), 
+		      "pet": random.choice(pet)}
+	return record
+
+def generateRandomSolution():
+	dlist = []
+	for i in xrange(0,5):
+		dlist.append(generateRandomRecord())
+	return dlist
+	
+print generateRandomSolution()

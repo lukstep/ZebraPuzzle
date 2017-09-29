3ce35ea3767d965a9c1a48ab7e291931908b812e
Lukasz <lukaszstepien02@gmail.com>
2017-09-29 21:21:31 +0200

    Initial commit

---
+++ zebraPuzzle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+nation = [ "Norwegian", "Ukrainian", "Englishman", "Spaniard", "Japanese" ]
+color  = [ "Red", "Blue", "Yellow", "Ivory", "Green" ]
+drink  = [ "Tea", "Milk", "Coffe", "Orange juice", "Water" ]
+smoke  = [ "Lucky Strike", "Old Gold", "Kools", "Chesterfield" ]
+pet    = [ "Zebra", "Fox", "Horse", "Snails", "Dog" ]
+
+

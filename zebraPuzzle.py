375e05cc349020a028c2b83addeeb724ff6df9cc
Lukasz <lukaszstepien02@gmail.com>
2017-09-30 08:56:23 +0200

    create class solution

--- zebraPuzzle.py
+++ zebraPuzzle.py
@@ -22,39 +22,74 @@ def generateRandomSolution():
         dlist.append(generateRandomRecord())
     return dlist
 
-def search(key, value, key2, value2, ilist):
-    p = 0
-    for p in ilist:
-        if p[key] == value and p[key2] == value2:
-            return p
+class solution():
 
-def getFitnes(solution):
-    fitnes = 0.00
-    if(solution[0]["nation"] == "Norwegian"):
-        fitnes = fitnes + 1
-    if(search("nation", "Englishman", "color", "Red", solution) != None):
-        fitnes = fitnes + 1
-    if(search("nation", "Spaniard", "pet", "Dog", solution) != None):
-        fitnes = fitnes + 1
-    if(search("drink", "Coffe", "color", "Green", solution) != None):
-        fitnes = fitnes + 1
-    if(search("nation", "Ukrainian", "drink", "Tea", solution) != None):
-        fitnes = fitnes + 1
-    #The green house is immediately to the right of the ivory house.
-    if(search("smoke", "Old Gold", "pet", "Snails", solution) != None):
-        fitnes = fitnes + 1
-    if(search("smoke", "Kools", "color", "Yellow", solution) != None):
-        fitnes = fitnes + 1
-    if(solution[2]["drink"] == "Milk"):
-        fitnes = fitnes + 1
-    #The man who smokes Chesterfields lives in the house next to the man with the fox
-    #Kools are smoked in the house next to the house where the horse is kept.
-    if(search("smoke", "Lucky Strike", "drink", "Orange juice", solution) != None):
-        fitnes = fitnes + 1
-    if(search("nation", "Japanese", "smoke", "Parliaments", solution) != None):
-        fitnes = fitnes + 1
-    fitnes = fitnes / 10
-    return fitnes
+    def __init__(self):
+        self.solutionTab = generateRandomSolution()
+        self.fitnes = 0.0
 
-print getFitnes(generateRandomSolution())
-#print search("drink", "Tea", generateRandomSolution())
+    def getFitnes(self):
+        self.live()
+        return self.fitnes
+
+    def getSolutionTab(self):
+        return self.solutionTab
+
+    def search(self, key, value, key2, value2):
+        p = 0
+        for p in self.solutionTab:
+            if p[key] == value and p[key2] == value2:
+                return True
+        return False
+
+    def live(self):
+        if(self.solutionTab[0]["nation"] == "Norwegian"):
+            self.fitnes += 1
+        if(self.search("nation", "Englishman", "color", "Red")):
+            self.fitnes += 1
+        if(self.search("nation", "Spaniard", "pet", "Dog")):
+            self.fitnes += 1
+        if(self.search("drink", "Coffe", "color", "Green")):
+            self.fitnes += 1
+        if(self.search("nation", "Ukrainian", "drink", "Tea")):
+            self.fitnes += 1
+        #The green house is immediately to the right of the ivory house.
+        if(self.search("smoke", "Old Gold", "pet", "Snails")):
+            self.fitnes += 1
+        if(self.search("smoke", "Kools", "color", "Yellow")):
+            self.fitnes += 1
+        if(self.solutionTab[2]["drink"] == "Milk"):
+            self.fitnes += 1
+        #The man who smokes Chesterfields lives in the house next to the man with the fox
+        #Kools are smoked in the house next to the house where the horse is kept.
+        if(self.search("smoke", "Lucky Strike", "drink", "Orange juice")):
+            self.fitnes += 1
+        if(self.search("nation", "Japanese", "smoke", "Parliaments")):
+            self.fitnes += 1
+        self.fitnes = self.fitnes / 10
+        #print self.fitnes
+
+def reproduce(solutionA, solutionB, pivot):
+    newSolution = [];
+    for i in range(5):
+        newSolution.append(solutionA[i])
+        if i < pivot:
+            newSolution.append(solutionB[i])
+    return newSolution
+
+def createPopulation(size):
+    population = []
+    for i in range(size):
+        s = solution()
+        population.append(s)
+        print s.getFitnes()
+        print s.getSolutionTab()
+    population.sort(key=lambda x: x.getFitnes(), reverse=True)
+    return population
+
+def run():
+    newPopulation = createPopulation(4)
+    oldPopulation = []
+
+#print getFitnes(generateRandomSolution()
+run()

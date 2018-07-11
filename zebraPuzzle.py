9d7dd709307243b658535d77202510ff9bd4097e
Lukasz Stepien <lukaszstepien02@gmail.com>
2018-07-11 14:36:55 +0200

    loging improvment

--- zebraPuzzle.py
+++ zebraPuzzle.py
@@ -3,13 +3,16 @@ import sys
 from population import *
 
 def run():
-    p = population(100000)
+    populationSize = 100000
+    p = population(populationSize)
+    print('Genetic Algorithm to solving zebra puzzle')
+    print('Population size: {size}' .format(size=populationSize))
     for i in range(100):
         p.reproduce()
         best = p.getBestFited()
-        print('Generation: {generation}, best fitnes: {fitnes}, average fitnes: {avg}'.format(generation=(i + 1), fitnes=best.getFitnes(), avg=p.getAverageFitnes()))
+        print('Generation: {generation}, best fitness: {fitnes}, average fitness: {avg}'.format(generation=(i + 1), fitnes=best.getFitnes(), avg=p.getAverageFitnes()))
         if(best.getFitnes() >= 100):
-            print('---BEST SOLUTION---')
+            print('Best solution')
             best.toString()
             sys.exit(0)
 

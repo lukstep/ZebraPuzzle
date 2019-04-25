01637079407469bcdcd184992a3e734daf60e802
Łukasz <lukaszstepien02@gmail.com>
2019-04-25 15:35:50 +0200

    Decreased number of generation

--- zebraPuzzle.py
+++ zebraPuzzle.py
@@ -7,7 +7,7 @@ def run():
     p = population(populationSize)
     print('Genetic Algorithm to solving zebra puzzle')
     print('Population size: {size}' .format(size=populationSize))
-    for i in range(100):
+    for i in range(5):
         p.reproduce()
         best = p.getBestFited()
         print('Generation: {generation}, best fitness: {fitnes}, average fitness: {avg}'.format(generation=(i + 1), fitnes=best.getFitnes(), avg=p.getAverageFitnes()))

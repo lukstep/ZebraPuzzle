939f339ee41d2765e7c037f20e2301a7bc2ab53e
Łukasz <lukaszstepien02@gmail.com>
2019-04-26 16:01:56 +0200

    Adede performace and stablity test

---
+++ performanceTest.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import time
+from population import population
+
+NUMBER_OF_GENERATIONS = 5
+NUMBER_OF_ALGORITM_EXECUTION = 10
+POPULATION_SIZE = 100000
+
+def runAlgorithm(populationSize):
+    p = population(populationSize)
+    for i in range(NUMBER_OF_GENERATIONS):
+        p.reproduce()
+        best = p.getBestFited()
+        if(best.getFitnes() >= 100):
+            return 1
+    return 0
+
+def benchmark():
+    print("Start performance and stability test")
+    NumberOfSucess = 0
+    startTime = time.process_time()
+    startPerfTime = time.perf_counter()
+
+    for i in range(NUMBER_OF_ALGORITM_EXECUTION):
+        if(runAlgorithm(POPULATION_SIZE)):
+            NumberOfSucess += 1
+
+    elapsedTime = time.process_time() - startTime
+    elapsedPerfTime = time.perf_counter() - startPerfTime
+    print('Test results: time: {time}s, perfTime: {perf}s, stability: {NumberOfSucess}/{ExeNumber}'.format(time=elapsedTime, perf=elapsedPerfTime, NumberOfSucess=NumberOfSucess, ExeNumber=NUMBER_OF_ALGORITM_EXECUTION))
+
+if __name__ == '__main__':
+   benchmark()
